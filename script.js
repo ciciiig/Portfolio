6cfa842b7cb90e64e00f930ed2aea4bfd7fda0bc
Gyo <tsintsadze7@gmail.com>
2021-07-01 11:39:21 +0400

    dark mode correction
    dark mode correction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// When the user scrolls below home, show the navbar.
+// When the user scrolls below home, show the navbar. Code below
 const nav = document.getElementById("nav");
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos+250 < window.innerHeight) {
+    if (prevScrollpos + 250 < window.innerHeight) {
         nav.style.visibility = 'hidden';
         nav.style.opacity = 0;
         nav.style.transition = 'visibility 0s linear 600ms, opacity 600ms';
@@ -14,4 +14,31 @@ window.onscroll = function () {
         nav.style.transition = 'visibility 0s linear 0s, opacity 600ms';
     }
     prevScrollpos = currentScrollPos;
+}
+// When the user scrolls below home, show the navbar. Code above
+
+// Toggle dark mode code below
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme)
+// Toggle dark mode code above
+
+// check local storage for theme color
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
