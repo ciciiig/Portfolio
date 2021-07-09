69798edd630c1638c97a32cc202f9984fb96c110
George <tsintsadze7@gmail.com>
2021-07-09 16:28:34 +0400

    init: portfolio ver2

---
+++ ver1/script.js
@@ -0,0 +1,49 @@
+// When the user scrolls below home, show the navbar or hamburger is media query is active. Code below
+const nav = document.getElementById("navbar");
+const hamburger = document.getElementById("hamburger-wrapper");
+var prevScrollpos = window.pageYOffset;
+
+function showAfterHome(elem) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < window.innerHeight) {
+        elem.style.visibility = 'hidden';
+        elem.style.opacity = 0;
+        elem.style.transition = 'visibility 0s linear 600ms, opacity 600ms';
+    } else {
+        elem.style.top = "0";
+        elem.style.visibility = 'visible';
+        elem.style.opacity = 1;
+        elem.style.transition = 'visibility 0s linear 0s, opacity 600ms';
+    }
+    prevScrollpos = currentScrollPos;
+}
+window.onscroll = function() {
+    showAfterHome(nav);
+    showAfterHome(hamburger);
+  }
+// When the user scrolls below home, show the navbar or hamburger is media query is active. Code above
+// Toggle dark mode code below
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light')        
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
+    }
+}
+toggleSwitch.addEventListener('change', switchTheme)
+// check local storage for theme color. Code below
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+// check local storage for theme color. Code above
+// Toggle dark mode code above
+
